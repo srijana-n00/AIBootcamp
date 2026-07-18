62fd8eaa65d27236a3ebae705596388695ef65e5
srijana-n00 <neupanesamjhana00@gmail.com>
2026-07-18 15:28:46 +0545

    Create armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,1 +1,12 @@
+def is_armstrong(num):
+    digits = [int(d) for d in str(num)]
+    num_digits = len(digits)
+    sum_of_powers = sum(d ** num_digits for d in digits)
+    return sum_of_powers == num
 
+
+number = int(input("Enter a number: "))
+if is_armstrong(number):
+    print(f"{number} is an Armstrong number")
+else:
+    print(f"{number} is not an Armstrong number")
