5fc9a7d4af8a75f9f74e1283cf5e1a5ab33ca9a2
srijana-n00 <neupanesamjhana00@gmail.com>
2026-07-18 16:20:16 +0545

    Add multiplication table

--- multitable.py
+++ multitable.py
@@ -1,1 +1,6 @@
-
+try:
+    num = int(input("Enter an integer: "))
+    for i in range(1, 11):
+        print(f"{num} x {i} = {num * i}")
+except ValueError:
+    print("Invalid input! Please enter an integer.")
