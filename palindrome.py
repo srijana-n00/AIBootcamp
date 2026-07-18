a68a8460277b208285e230e44cceac63b596736b
srijana-n00 <neupanesamjhana00@gmail.com>
2026-07-18 15:54:36 +0545

    Add palindrome checker function

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,9 @@
+def is_palindrome(n):
+    str_num = str(n)
+    return str_num == str_num[::-1]
 
+num = int(input("Enter a number: "))
+if is_palindrome(num):
+    print(f"{num} is a palindrome")
+else:
+    print(f"{num} is not a palindrome")
